cf19a8e05ddedf50b648535ba005198bc52ac525
sswaroop <sswaroop@DIN73002985.corp.capgemini.com>
2018-08-06 14:53:58 +0530

    added sum

--- Day1-GitDemo/src/main/java/org/cap/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/SalesClass.java
@@ -3,8 +3,11 @@ package org.cap;
 public class SalesClass {
 int[] arr= {1,2,3,4,5};
 	public void show() {
+		int s=0;
 		for(int num:arr) {
 			System.out.println(num);
+			s+=num;
 		}
+		System.out.println(s);
 	}
 }
