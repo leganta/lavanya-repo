ff50104fb2388d23c097e6b450d59dfe27fd733d
leganta <leganta@DIN73002721.corp.capgemini.com>
2018-08-06 12:25:23 +0530

    Price details are added successfully

--- Day1-GitDemo/src/main/java/org/cap/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/MainClass.java
@@ -7,10 +7,12 @@ public class MainClass {
 	public static void main(String[] args) {
 		SalesClass class1=new SalesClass();
 		ProductDetails class2=new ProductDetails();
+		PriceDetails class3=new PriceDetails();
 		System.out.println("Hello World!............");
 		greet();
 class1.show();
 class2.review();
+class3.details();
 	}
 public static void greet() {
 	System.out.println("helloo..........");
---
+++ Day1-GitDemo/src/main/java/org/cap/PriceDetails.java
@@ -0,0 +1,10 @@
+package org.cap;
+
+public class PriceDetails {
+
+	public void details() {
+		System.out.println("soap=10");
+		System.out.println("paste=20");
+		System.out.println("Electronics=1000");
+	}
+}
