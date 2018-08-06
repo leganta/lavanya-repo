552c4447e1d80987a4a1bc06e62f347ce7cdf8c3
leganta <leganta@DIN73002721.corp.capgemini.com>
2018-08-06 12:20:04 +0530

    Product Details are added

--- Day1-GitDemo/src/main/java/org/cap/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/MainClass.java
@@ -6,9 +6,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		SalesClass class1=new SalesClass();
+		ProductDetails class2=new ProductDetails();
 		System.out.println("Hello World!............");
 		greet();
 class1.show();
+class2.review();
 	}
 public static void greet() {
 	System.out.println("helloo..........");
---
+++ Day1-GitDemo/src/main/java/org/cap/ProductDetails.java
@@ -0,0 +1,9 @@
+package org.cap;
+
+public class ProductDetails {
+	
+	public void review() {
+		System.out.println("Ganta");
+		System.out.println("Leela Lavanya");
+	}
+}
