c57aa8b3d7a0edeeeee8e475003b3925ae770993
leganta <leganta@DIN73002721.corp.capgemini.com>
2018-08-06 10:41:20 +0530

    Commit All

---
+++ Day1-GitDemo/src/main/java/org/cap/MainClass.java
@@ -0,0 +1,16 @@
+package org.cap;
+
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!............");
+		greet();
+
+	}
+public static void greet() {
+	System.out.println("helloo..........");
+}
+}
