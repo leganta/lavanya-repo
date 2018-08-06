c0a493499c4f50a9385d04a2652ce50f1740a70c
leganta <leganta@DIN73002721.corp.capgemini.com>
2018-08-06 12:11:59 +0530

    Sales are added

--- Day1-GitDemo/src/main/java/org/cap/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/MainClass.java
@@ -5,10 +5,10 @@ package org.cap;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+		SalesClass class1=new SalesClass();
 		System.out.println("Hello World!............");
 		greet();
-
+class1.show();
 	}
 public static void greet() {
 	System.out.println("helloo..........");
---
+++ Day1-GitDemo/src/main/java/org/cap/SalesClass.java
@@ -0,0 +1,10 @@
+package org.cap;
+
+public class SalesClass {
+int[] arr= {1,2,3,4,5};
+	public void show() {
+		for(int num:arr) {
+			System.out.println(num);
+		}
+	}
+}
